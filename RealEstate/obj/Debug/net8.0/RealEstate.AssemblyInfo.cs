41cf534cc54d451bfdee5156c8374b261039c89c
Fabbernat <fabbernat@gmail.com>
2026-05-27 19:32:57 +0200

    sd :qa :q q !wqa
    :wq

--- RealEstate/obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
+++ RealEstate/obj/Debug/net8.0/RealEstate.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85f3343086f8bb0a1a6864157be62b08c0728684")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
